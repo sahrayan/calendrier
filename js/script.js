9a62b3192024ab1ba62ffeb4eaa0be2af8b68062
Rayan R <rayanraihani9@gmail.com>
2022-12-02 16:15:34 +0100

    rajouter l'option click pour ouvrire la case + verif du code

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const title = document.querySelector("");
+console.log(title)
+
+const divBoxes = document.querySelector("#boxes");
+console.log(divBoxes)
+
+const boxes = document.querySelectorAll(".box");
+console.log(boxes)
